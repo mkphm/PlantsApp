4de6e0696aff2faf77a163c3b315a9ac709af7c4
mkphm <khuepham1403@gmail.com>
2022-02-14 08:00:13 -0800

    add array

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let noteArray = [];
+let plantArray = [];
 let selectedType = "";
 
 // define a constructor to create note objects
-let NoteObject = function (pData, pYear, pType) {
+let NoteObject = function (pData, pPlant, pType) {
     this.data = pData;
-    this.year = pYear;
+    this.plant = pPlant;
     this.type = pType;
 }
 
@@ -14,10 +14,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("buttonAdd").addEventListener("click", function () {
 
-        noteArray.push(new NoteObject(document.getElementById("note").value, document.getElementById("year").value, selectedType));
-        console.log(noteArray);
+        plantArray.push(new NoteObject(document.getElementById("note").value, document.getElementById("plant").value, selectedType));
+        console.log(plantArray);
         document.getElementById("note").value = "";
-        document.getElementById("year").value = "";
+        document.getElementById("plant").value = "";
     });
 
     $(document).bind("change", "#select-type", function (event, ui) {
@@ -40,9 +40,36 @@ function createList() {
     var myul = document.getElementById("myList");
     myul.innerHTML = '';
 
-    noteArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-        li.innerHTML = element.data + " - " + element.year + ". Genre: "+ element.type;
-        myul.appendChild(li);
+    // movieArray.forEach(function (element,) {   // use handy array forEach method
+    //     var li = document.createElement('li');
+    //     li.innerHTML = element.data + " - " + element.plant + ". Genre: "+ element.type;
+    //     myul.appendChild(li);
+    // });
+
+    plantArray.forEach(function(element, i) {
+        var myLi = document.createElement("li");
+        myLi.classList.add('onePlant');
+        myLi.innerHTML = plantArray[i].ID + ": " + element.data + " " + element.type + " counter: " + i;
+
+        myLi.setAttribute("data-parm", element.ID);
+
+        myul.appendChild(myLi);
     });
+
+    var liList = document.getElementsByClassName("onePlant");
+    let newPlantArray = Array.from(liList);
+    newPlantArray.forEach(function (element, i) {
+        element.addEventListener('click', function() {
+            var parm = this.getAttribute("data-parm");
+            localStorage.setItem('parm', parm);
+            document.location.href = "index.html#details";
+        })
+    })
+
+
+
+
+
+
+
 };
